7cc0301b60865307b1ca5b065f3914d2cc5cbc28
RD <rdclare75@gmail.com>
2019-12-02 16:43:12 -0700

    start on autocorrect

---
+++ app/src/main/java/com/javafinal/shm/tapAutocorr.java
@@ -0,0 +1,12 @@
+package com.javafinal.shm;
+
+import java.util.*;
+
+public class tapAutocorr {
+    public static void main(String args[]){
+        Map< String, String> dict = new HashMap< String, String>();
+        dict.put("hl","hello");
+
+    }
+
+}
