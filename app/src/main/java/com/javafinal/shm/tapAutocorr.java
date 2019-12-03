d5d392a8d83e3053c34f107f81abcd24f4148736
RD <rdclare75@gmail.com>
2019-12-03 13:39:39 -0700

    update to auto corrector

--- app/src/main/java/com/javafinal/shm/tapAutocorr.java
+++ app/src/main/java/com/javafinal/shm/tapAutocorr.java
@@ -1,12 +1,33 @@
 package com.javafinal.shm;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 
 public class tapAutocorr {
-    public static void main(String args[]){
+    public static void main(String[] args){
         Map< String, String> dict = new HashMap< String, String>();
         dict.put("hl","hello");
-
+        File file = new File("dict.txt");
+        try{
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                String line = scanner.nextLine();
+                if(scanner.hasNextLine()) {
+                    String line2 = scanner.nextLine();
+                    dict.put(line, line2);
+                }
+                //dict.put(line,"test");
+                //System.out.println(line);
+            }
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        Set< Map.Entry< String, String> > st = dict.entrySet();
+        for(Map.Entry< String,String> me:st){
+            System.out.print(me.getKey()+":");
+            System.out.println(me.getValue());
+        }
     }
 
 }
