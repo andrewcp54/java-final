493cbc1ee1f23aebb5263a7b2f3103592ef2c186
andrewcp54 <andrewcp540@gmail.com>
2019-12-09 11:35:44 -0700

    Update files

--- app/src/main/java/com/javafinal/shm/MainActivity.java
+++ app/src/main/java/com/javafinal/shm/MainActivity.java
@@ -4,29 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn;
+    private EditText et;
+    String s;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn =findViewById(R.id.chatbutton);
+        btn = findViewById(R.id.chatbutton);
+        et = findViewById(R.id.editTest);
+
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                s = et.getText().toString();
+                if (s.equals("GL")) {
+                    et.setText("Good luck");
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                chattest();
+                chat_test();
             }
         });
     }
-    private void chattest(){
-    Intent myIntent = new Intent(MainActivity.this, chat.class);
-    startActivity(myIntent);
+
+    private void chat_test(){
+        Intent myIntent = new Intent(MainActivity.this, chat.class);
+        startActivity(myIntent);
     }
 }
 
