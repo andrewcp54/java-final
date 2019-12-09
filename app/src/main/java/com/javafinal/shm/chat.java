ee58b95b6aa391bce1946a6a071efb895c1bb4f8
andrewcp54 <andrewcp540@gmail.com>
2019-12-09 15:45:13 -0700

    Update chat.java

--- app/src/main/java/com/javafinal/shm/chat.java
+++ app/src/main/java/com/javafinal/shm/chat.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scaledrone.lib.Listener;
+import com.scaledrone.lib.Member;
 import com.scaledrone.lib.Room;
 import com.scaledrone.lib.RoomListener;
 import com.scaledrone.lib.Scaledrone;
@@ -19,33 +22,8 @@ public class chat extends AppCompatActivity implements RoomListener {
     private EditText sms;
     private Scaledrone sd;
     private String channel_ID = "XePiiGhjEAIdU07z", roomName = "observable-first_room";
-
-    private String getRandomName() {
-        String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
-        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
-        return (
-                adjs[(int) Math.floor(Math.random() * adjs.length)] +
-                        "_" +
-                        nouns[(int) Math.floor(Math.random() * nouns.length)]
-        );
-    }
-
-    private String getRandomColor() {
-        Random r = new Random();
-        StringBuffer sb = new StringBuffer("#");
-        while(sb.length() < 7){
-            sb.append(Integer.toHexString(r.nextInt()));
-        }
-        return sb.toString().substring(0, 7);
-    }
-
-    public void sendMessage(View view) {
-        String message = sms.getText().toString();
-        if (message.length() > 0) {
-            sd.publish(roomName, message);
-            sms.getText().clear();
-        }
-    }
+    private msgAdapter msg_a;
+    private ListView msgs_view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +31,10 @@ public class chat extends AppCompatActivity implements RoomListener {
         setContentView(R.layout.activity_chat);
 
         sms = findViewById(R.id.sms_body);
+
+        msg_a = new msgAdapter(this);
+        msgs_view = findViewById(R.id.chat_view);
+        msgs_view.setAdapter(msg_a);
 
         MemberData data = new MemberData(getRandomName(), getRandomColor());
 
@@ -81,6 +63,14 @@ public class chat extends AppCompatActivity implements RoomListener {
         });
     }
 
+    public void sendMessage(View view) {
+        String message = sms.getText().toString();
+        if (message.length() > 0) {
+            sd.publish(roomName, message);
+            sms.getText().clear();
+        }
+    }
+
     @Override
     public void onOpen(Room room) {
         System.out.println("Connected to room successfully!");
@@ -93,6 +83,7 @@ public class chat extends AppCompatActivity implements RoomListener {
 
     @Override
     public void onMessage(Room room, com.scaledrone.lib.Message receivedMessage) {
+        System.out.println("Sent message!");
         final ObjectMapper mapper = new ObjectMapper();
         try {
             final MemberData data = mapper.treeToValue(receivedMessage.getMember().getClientData(), MemberData.class);
@@ -101,9 +92,8 @@ public class chat extends AppCompatActivity implements RoomListener {
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
-                    msgAdapter msg_a;
-                    //msg_a.add(message);
-                    //.setSelection(messagesView.getCount() - 1);
+                    msg_a.add(message);
+                    msgs_view.setSelection(msgs_view.getCount() - 1);
                 }
             });
         } catch (JsonProcessingException e) {
@@ -111,6 +101,24 @@ public class chat extends AppCompatActivity implements RoomListener {
         }
     }
 
+    private String getRandomName() {
+        String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
+        String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
+        return (
+                adjs[(int) Math.floor(Math.random() * adjs.length)] +
+                        "_" +
+                        nouns[(int) Math.floor(Math.random() * nouns.length)]
+        );
+    }
+
+    private String getRandomColor() {
+        Random r = new Random();
+        StringBuffer sb = new StringBuffer("#");
+        while(sb.length() < 7){
+            sb.append(Integer.toHexString(r.nextInt()));
+        }
+        return sb.toString().substring(0, 7);
+    }
 }
 
 class MemberData {
@@ -122,11 +130,22 @@ class MemberData {
         this.color = color;
     }
 
+    public MemberData() {
+    }
+
     public String getName() {
         return name;
     }
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberData{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
